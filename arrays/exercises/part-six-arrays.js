811fc0e9e41a65d89069dbd7775c210a35c52cea
CandyQueen77 <angiepolitte@gmail.com>
2024-05-07 14:02:36 -0500

    Arrays parts 1-6

--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -1,11 +1,51 @@
 //Arrays can hold different data types, even other arrays! A multi-dimensional array is one with entries that are themselves arrays.
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
-
+let element1 = ['hydrogen', 'H', 1.008];
+let element2 = ['helium', 'He', 4.003];
+let element26 = ['iron', 'Fe', 55.85];
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
+let tableOfElements = [];
+tableOfElements.push(element1);
+tableOfElements.push(element2);
+tableOfElements.push(element26);
+
+console.log(tableOfElements);
+
 
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
-
+console.log(tableOfElements[1], tableOfElements[1][1]);
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
+console.log(tableOfElements[0][2], tableOfElements[1][0], tableOfElements[2][1]);
+
 
 //5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+let elementalTable = [];
+let elementName = [];
+let elementWeight = [];
+let elementSymbol = [];
+
+elementName.push(element1[0]);
+elementName.push(element2[0]);
+elementName.push(element26[0]);
+console.log(elementName);
+
+elementWeight.push(element1[2]);
+elementWeight.push(element2[2]);
+elementWeight.push(element26[2]);
+console.log(elementWeight);
+
+elementSymbol.push(element1[1]);
+elementSymbol.push(element2[1]);
+elementSymbol.push(element26[1]);
+console.log(elementSymbol);
+
+
+elementalTable.push(elementName);
+elementalTable.push(elementWeight);
+elementalTable.push(elementSymbol);
+console.log(elementalTable);
+
+
+
+
