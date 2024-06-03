d7c6f3865c8160bb8fa4f8d9c3ca1f1d328c05fe
CandyQueen77 <angiepolitte@gmail.com>
2024-06-02 19:41:07 -0500

    Objects and Math exercises

--- objects-and-math/chapter-examples/ForInLoop.js
+++ objects-and-math/chapter-examples/ForInLoop.js
@@ -5,5 +5,12 @@ let tortoiseOne = {
    age: 85,
    diet: ["pumpkins", "lettuce", "cabbage"]
 };
+let numbers = [-2, 3.33, -4.44, 8.88];
+
+let sortedArray = numbers.sort(function(a, b){return a-b});
+
+console.log(sortedArray);
+console.log(`Min = ${sortedArray[0]}, Max = ${sortedArray[sortedArray.length-1]}`);
+
 
 // Using a for..in loop, iterate through each property in the tortoiseOne object and print the value to the console.
--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -1,21 +1,80 @@
 let superChimpOne = {
-   name: "Chad",
+   callSign: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 3,
+   move: function() {return Math.floor(Math.random()*11)}
 };
 
 let salamander = {
-   name: "Lacey",
+   callSign: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 4,
+   move: function() {return Math.floor(Math.random()*11)}
 };
+
+let superChimpTwo = {
+   callSign: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 7,
+   move: function() {return Math.floor(Math.random()*11)}
+};
+
+let puppyDog = {
+   callSign: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 6,
+   move: function() {return Math.floor(Math.random()*11)}
+};
+
+let waterBear = {
+   callSign: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 8,
+   move: function() {return Math.floor(Math.random()*11)}
+};
+let animalCrew = [superChimpOne, salamander, superChimpTwo, puppyDog, waterBear];
+
+let fitnessTest = function(animalCrew) {
+   let results = [], numSteps, turns;
+   for (let i = 0; i < animalCrew.length; i++){
+       numSteps = 0;
+       turns = 0;
+       while(numSteps < 20){
+       numSteps += animalCrew[i].move();
+       turns++;
+       }
+       results.push(`${animalCrew[i].callSign} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+  }
+  console.log(fitnessTest(animalCrew));
+
+
+// function crewReports(animal) {
+//    console.log(`${animal.callSign} is a ${animal.species}. They are ${animal.age} years old and ${animal.mass} kilograms. Their ID is ${animal.astronautID}.`);
+// }
+//console.log(crewReports(waterBear));
 
 
 // After you have created the other object literals, add the astronautID property to each one.
 
+// for (i = 0; i < 5; i++) {
+//    let numID = Math.ceil(Math.random()*10);
+//    console.log(num);
+// }
+
 // Add a move method to each animal object
+
 
 // Create an array to hold the animal objects.
 
