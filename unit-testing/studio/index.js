daabe33f63fc65c04dac93d840a187297ff79281
CandyQueen77 <angiepolitte@gmail.com>
2024-06-21 21:40:23 -0500

    Unit Testing Studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,60 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ['Web Development', 'Data Analysis', 'Liftoff'],
+  launchOutput: (num) => {
+    // if (num % 2 === 0) {
+    //     return "Launch!";
+    // }
+    // if (num % 3 === 0) {
+    //     return "Code!";
+    // }
+    // if (num % 5 === 0) {
+    //     return "Rocks!";
+    // }
+    // if (num % 2 === 0 && num % 3 === 0) {
+    //     return "LaunchCode!";
+    //     } else if (num % 2 === 0) {
+    //         return "Launch!";
+    //     } else if (num % 3 === 0) {
+    //         return "Code!";
+    //     }
+    // if (num % 3 === 0 && num % 5 === 0) {
+    //     return "Code Rocks!";
+    // } else if (num % 3 === 0) {
+    //         return "Code!";
+    // } else if (num % 5 === 0) {
+    //         return "Rocks!";
+    //     }
+    // if (num % 2 === 0 && num % 5 === 0) {
+    //     return "Launch Rocks!";
+    // } else if (num % 2 === 0) {
+    //     return "Launch!";
+    // } else if (num % 5 === 0) {
+    //     return "Rocks!";
+    // }
+    if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+        return "LaunchCode Rocks!";
+    } else if (num % 2 === 0 && num % 5 === 0) {
+        return "Launch Rocks! (CRASH!!!!)";
+    } else if (num % 3 === 0 && num % 5 === 0) {
+        return "Code Rocks!";
+    } else if (num % 2 === 0 && num % 3 === 0) {
+        return "LaunchCode!";
+    } else if (num % 2 === 0) {
+        return "Launch!";
+    } else if (num % 3 === 0) {
+        return "Code!";
+    } else if (num % 5 === 0) {
+        return "Rocks!";
+    } return "boo.";
+    }
 }
+
+console.log(launchcode.launchOutput(10));
+
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -3,6 +3,58 @@ const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
 
-  // Write your unit tests here!
+  test("should return 'nonprofit' for organization key", () => {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+
+  test("should return 'Jeff' for executiveDirector key", () => {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("should return 100 for percentageCoolEmployees key", () => {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+  test("should return 'Web Development', 'Data Analysis', and 'Liftoff' as items in programsOffered", () => {
+    expect(launchcode.programsOffered[0]).toBe("Web Development");
+    expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.programsOffered[2]).toBe("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
   
 });
+
+describe("Testing launchOutput", function() {
+
+  test("should return 'Launch' when num is even", () => {
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+   });
+
+   test("should return 'Code!' when num is divisible by three", () => {
+    expect(launchcode.launchOutput(3)).toBe("Code!");
+   });
+
+   test("should return 'Rocks!' when num is divisible by 5", () => {
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+   });
+
+   test("should return 'LaunchCode!' when a number is divisible by 2 AND 3", () => {
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+   });
+
+   test("should return 'Code Rocks!' when a number is divisible by 3 AND 5", () => {
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+   });
+
+   test("should return 'Launch Rocks! (CRASH!!!!)' when a number is divisible by 2 AND 5", () => {
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks! (CRASH!!!!)");
+   });
+
+   test("should return 'LaunchCode Rocks!' when number is divisible by 2 AND 3 AND 5", () => {
+    expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!");
+   });
+  
+})
+
+
+
